75a3b8c9bca43a7a6d3818d5adf43c24bb4897ad
Jinping Wu <jinping.wu@verisilicon.com>
2022-04-17 19:32:18 +0800

    thomas-m3: first version

---
+++ rungdb.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# shell folder
+shell_folder=$(cd "$(dirname "$0")" || exit;pwd)
+workspace_dir=${shell_folder}/../../..
+
+# toolchain
+toolchains_try_dir1=${workspace_dir}/.toolchains;
+toolchains_try_dir2=~/.toolchain;
+toolchian=gcc-arm-none-eabi-10.3-2021.10
+
+# which demo to compile
+demo_dir=${shell_folder}/FreeRTOS/Demo/THOMAS_M3_QEMU
+
+# get toolchain and export
+function get_and_export_toolchain()
+{
+    echo "get_toolchain"
+    if [[ -d ${toolchains_try_dir1} ]]; then
+        if [[ -d ${toolchains_try_dir1}/${toolchian}/bin ]]; then
+            echo "find toolchain from ${toolchains_try_dir1}"
+            export PATH="${toolchains_try_dir1}/${toolchian}/bin:$PATH"
+            return
+        fi
+    fi
+    
+    if [[ -d ${toolchains_try_dir2} ]]; then
+        if [[ -d ${toolchains_try_dir2}/${toolchian}/bin ]]; then
+            echo "find toolchain from ${toolchains_try_dir2}"
+            export PATH="${toolchains_try_dir2}/${toolchian}/bin:$PATH"
+            return
+        fi
+    fi
+    echo "cann't find toolchain, please run root script './build.sh --toolchains' ,exit ...."
+    exit
+}
+
+get_and_export_toolchain
+
+# gdb
+arm-none-eabi-gdb \
+-ex 'target remote localhost:1234' \
+-ex "add-symbol-file ${demo_dir}/build/RTOSDemo.axf" \
+-q 
---
+++ runqemu.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# shell folder
+shell_folder=$(cd "$(dirname "$0")" || exit;pwd)
+workspace_dir=${shell_folder}/../../..
+
+qemu_version=qemu-6.0.0
+
+# which demo to compile
+demo_dir=${shell_folder}/FreeRTOS/Demo/THOMAS_M3_QEMU
+
+# toolchain
+qemu_try_dir1=${workspace_dir}/software/qemu
+qemu_try_dir2=~/.toolchain/qemu
+
+# get toolchain and export
+function get_and_export_qemu()
+{
+    echo "get qemu"
+    if [[ -d ${qemu_try_dir1} ]]; then
+        if [[ -d ${qemu_try_dir1}/${qemu_version}/build ]]; then
+            echo "find qemu from ${qemu_try_dir1}"
+            export PATH="${qemu_try_dir1}/${qemu_version}/build:$PATH"
+            return
+        fi
+    fi
+
+    if [[ -d ${qemu_try_dir2} ]]; then
+        if [[ -d ${qemu_try_dir2}/${qemu_version}/build ]]; then
+            echo "find qemu from ${qemu_try_dir2}"
+            export PATH="${qemu_try_dir2}/${qemu_version}/build:$PATH"
+            return
+        fi
+    fi
+    
+    echo "cann't find qemu, please run root script './build.sh --qemu' ,exit ...."
+    exit
+}
+
+get_and_export_qemu
+
+qemu_option=
+if [[ $1  = "--gdb" ]]; then
+    qemu_option+=" -s -S"
+    echo "enable gdb, please run script './rungdb', and enter c "
+else
+    echo "not use gdb, just run"
+fi
+
+qemu_option+=" -machine thomas-m3"
+qemu_option+=" -kernel ${demo_dir}/build/RTOSDemo.axf"
+qemu_option+=" -serial stdio"
+
+# run qemu
+../qemu/build/qemu-system-arm ${qemu_option}
+
